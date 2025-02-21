0beed9160dea3d8aac754751a86d374e2b8913f7
Arjunchouhann <chouhanarjun05112003@gmail.com>
2025-02-21 16:29:57 +0530

    [Arjun] Add.Welcoming program by HTML

--- src/main/java/com/spring_intro/hello_world/controller/UserController.java
+++ src/main/java/com/spring_intro/hello_world/controller/UserController.java
@@ -1,13 +1,21 @@
 package com.spring_intro.hello_world.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//Controller Class
+@Controller
 public class UserController {
 
     @GetMapping("/hello")
-    public String sayHello(){
-        return "Hello From BridgeLabz";
+    public String sayHello() {
+        return "Hello From BridgeLabz"; //return Hello message
+    }
+
+    @GetMapping("/hello/message")
+    public String message(Model model) {
+        model.addAttribute("message", "Hello from BridgeLabz");
+        return "index"; // returns index.html
     }
 }
